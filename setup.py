1673648d5826e264e02d55e2b520fe6f39bcd4d8
swiggumj <swiggumj@gmail.com>
2020-11-12 14:05:21 -0600

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="timing_analysis",
+    version="0.1.0",
+    description="NANOGrav Timimg Analysis",
+    author="Joe Swiggum",
+    author_email="swiggumj@gmail.com",
+    url="",
+    packages=find_packages(where='.'),
+)
