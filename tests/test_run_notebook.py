399b023fc3e630fb23285d3d80085dda9076eefc
Ross Jennings <rossjjennings@gmail.com>
2020-12-18 22:07:46 +0000

    Make test_run_notebook pytest friendly

--- tests/test_run_notebook.py
+++ tests/test_run_notebook.py
@@ -6,16 +6,28 @@ import pint.fitter
 from os.path import dirname, join
 from multiprocessing import Pool
 import traceback
+from glob import glob
+import pytest
 
 global_log = 'test-run-notebooks.log'
 
-def test_run_notebook(config_file, log_file):
+def config_files():
+    base_dir = dirname(dirname(__file__))
+    config_files = (glob(join(base_dir, 'configs/B*.nb.yaml'))
+                     + glob(join(base_dir, 'configs/J*.nb.yaml')))
+    config_file = sorted(config_files)
+    return config_files
+
+@pytest.mark.parametrize('config_file', config_files())
+def test_run_notebook(config_file, suppress_errors=False):
     """
     Run through the basic set of functions that will be called in the notebook for each pulsar.
     For now, this must be run from the top-level `timing-analysis` directory.
     It will create a global log called test-run-notebooks.log, and a log file for each pulsar.
     """
     log.setLevel("DEBUG")
+    cfg_name = '.'.join(config_file.split('/')[-1].split('.')[:-1])
+    log_file = f'{cfg_name}.log'
 
     # clear log file
     with open(log_file, 'w') as f:
@@ -43,23 +55,17 @@ def test_run_notebook(config_file, log_file):
 
             with open(global_log, 'a') as f:
                 print(f"{config_file}: failure - {repr(e)}", file=f)
+            if not suppress_errors:
+                raise e
 
 if __name__ == '__main__':
-    from glob import glob
-    
-    base_dir = dirname(dirname(__file__))
-    config_files = (glob(join(base_dir, 'configs/B*.nb.yaml'))
-                     + glob(join(base_dir, 'configs/J*.nb.yaml')))
-
     # clear global log
     with open(global_log, 'w') as f:
         pass
 
     with Pool(processes=4) as pool:
         results = []
-        for config_file in sorted(config_files):
-            cfg_name = '.'.join(config_file.split('/')[-1].split('.')[:-1])
-            log_file = f'{cfg_name}.log'
-            results.append(pool.apply_async(test_run_notebook, (config_file, log_file)))
+        for config_file in config_files():
+            results.append(pool.apply_async(test_run_notebook, (config_file,), {'suppress_errors': True}))
         for result in results:
             result.get()
