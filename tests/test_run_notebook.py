b93208f44557a0e08cab8f0df1597d83ac5735b0
Joe Glaser <astroglaser@gmail.com>
2021-03-17 15:13:12 -0400

    Putting things in the correct place.

--- tests/test_run_notebook.py
+++ tests/test_run_notebook.py
@@ -104,7 +104,7 @@ if __name__ == '__main__':
     with open(global_log, 'w') as f:
         pass
 
-    with Pool(processes=1) as pool:
+    with Pool(processes=2) as pool:
         code = notebook_code.__wrapped__()
         results = []
         for config_file in config_files():
