640163c5628829ea9e66bc45a02e0df25f76dedd
Ross Jennings <rossjjennings@gmail.com>
2021-03-18 21:34:55 +0000

    Add logic for capturing errors

--- src/timing_analysis/notebook_runner.py
+++ src/timing_analysis/notebook_runner.py
@@ -5,9 +5,9 @@ from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
 import timing_analysis
 from timing_analysis.notebook_templater import transform_notebook
 
-base_dir = os.path.dirname(os.path.dirname(timing_analysis.__path__))
+base_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
-def run_notebook(template_nb, output_nb, workdir=base_dir, transformations=None):
+def run_notebook(template_nb, output_nb, err_file, workdir=base_dir, transformations=None):
     with open(template_nb) as f:
         nb = nbformat.read(f, as_version=4)
     if transformations is not None:
@@ -17,7 +17,9 @@ def run_notebook(template_nb, output_nb, workdir=base_dir, transformations=None)
     try:
         ep.preprocess(nb, {'metadata': {'path': workdir}})
     except CellExecutionError as err:
-        print(err.traceback)
+        with open(err_file, 'w') as f:
+            print(err.traceback, file=f)
+        raise err
     finally:
         with open(output_nb, 'w', encoding='utf-8') as f:
             nbformat.write(nb, f)
