8e4ee1b57a9bfb91e9ccdd3f0b1b6d340e780688
Ross Jennings <rossjjennings@gmail.com>
2021-03-18 21:55:59 +0000

    Add logic for logging to file

--- src/timing_analysis/notebook_runner.py
+++ src/timing_analysis/notebook_runner.py
@@ -1,5 +1,6 @@
 import os
 import nbformat
+import textwrap
 from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
 
 import timing_analysis
@@ -7,11 +8,19 @@ from timing_analysis.notebook_templater import transform_notebook
 
 base_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
-def run_notebook(template_nb, output_nb, err_file, workdir=base_dir, transformations=None):
+def run_notebook(template_nb, output_nb, log_file, err_file, workdir=base_dir, transformations=None):
     with open(template_nb) as f:
         nb = nbformat.read(f, as_version=4)
     if transformations is not None:
         n_subs = transform_notebook(nb, transformations)
+    
+    for cell in nb['cells']:
+        if 'tags' in cell['metadata'] and 'logging' in cell['metadata']['tags']:
+            cell['source'] = f'''
+            log.setLevel("INFO")
+            log.log_to_file("{log_file}")
+            '''
+            cell['source'] = textwrap.dedent(cell['source']).strip()
     
     ep = ExecutePreprocessor()
     try:
