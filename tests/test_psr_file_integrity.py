316db7b84c4076e96d88615ea796c02197f557a2
Anne Archibald <anne.archibald@ncl.ac.uk>
2021-03-22 21:09:38 +0000

    Improve consistency test, turn it on always

--- tests/test_psr_file_integrity.py
+++ tests/test_psr_file_integrity.py
@@ -1,8 +1,10 @@
 from os.path import join, exists
 from glob import glob
 import yaml
+import pytest
 
-def test_psr_file_integrity():
+@pytest.fixture(scope="module")
+def find_bad():
     bad_yamls = {}
     results = {p:[] for p in glob("results/*.par")}
     bad_results = {}
@@ -27,23 +29,26 @@ def test_psr_file_integrity():
             if not exists(ap):
                 bad_comparisons[c] = ap
 
+    return bad_yamls, bad_results, bad_comparisons, results
+
+def test_yaml_ok(find_bad):
+    bad_yamls, _, _, _ = find_bad
     if bad_yamls:
         raise ValueError(f"Some yamls do not parse correctly: {bad_yamls}")
-    else:
-        print("All YAML files are stored and parse correctly.")
 
+def test_par_files_exist(find_bad):
+    _, bad_results, _, _ = find_bad
     if bad_results:
         raise ValueError(f"Some par files are referenced but do not exist: {bad_results}")
-    else:
-        print("All PAR files referenced exist in the correct directory.")
 
+def test_compare_par_files_exist(find_bad):
+    _, _, bad_comparisons, _ = find_bad
     if bad_comparisons:
         raise ValueError(f"Some comparison par files are referenced but do not exist: {bad_comparisons}")
-    else:
-        print("All referenced comparison PAR files exist in the correct directory.")
 
+def test_par_files_not_shared(find_bad):
+    _, _, _, results = find_bad
     wrong_number = {r:l for r,l in results.items() if len(l)!=1}
     if wrong_number:
         raise ValueError(f"Some par files are referenced by no or more than one config yaml: {wrong_number}")
-    else:
-        print("All referenced PAR files are connected to a single config YAML.")
+
