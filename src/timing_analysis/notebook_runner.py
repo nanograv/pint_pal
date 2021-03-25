85dd8c2442121cda40b4346fe27244b0c27f6b57
Ross Jennings <rossjjennings@gmail.com>
2021-03-25 19:21:45 +0000

    Allow error logging to be skipped

--- src/timing_analysis/notebook_runner.py
+++ src/timing_analysis/notebook_runner.py
@@ -10,7 +10,7 @@ from timing_analysis.notebook_templater import transform_notebook
 base_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 ansi_color = re.compile(r'\x1b\[([0-9]{1,3};)*[0-9]{1,3}m')
 
-def run_notebook(template_nb, output_nb, err_file, workdir=base_dir, color_err=False, transformations=None):
+def run_notebook(template_nb, output_nb, err_file=None, workdir=base_dir, color_err=False, transformations=None):
     with open(template_nb) as f:
         nb = nbformat.read(f, as_version=4)
     if transformations is not None:
@@ -20,10 +20,11 @@ def run_notebook(template_nb, output_nb, err_file, workdir=base_dir, color_err=F
     try:
         ep.preprocess(nb, {'metadata': {'path': workdir}})
     except CellExecutionError as err:
-        with open(err_file, 'w') as f:
-            if not color_err:
-                traceback = re.sub(ansi_color, '', err.traceback)
-            print(traceback, file=f)
+        if err_file is not None:
+            with open(err_file, 'w') as f:
+                if not color_err:
+                    traceback = re.sub(ansi_color, '', err.traceback)
+                print(traceback, file=f)
         raise err
     finally:
         with open(output_nb, 'w', encoding='utf-8') as f:
