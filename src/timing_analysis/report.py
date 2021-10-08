789021e7e3a0d5a90e41d5e4d8d7056118c43c14
Anne Archibald <anne.archibald@ncl.ac.uk>
2021-10-08 17:31:14 +0000

    Initial implementation

---
+++ src/timing_analysis/report.py
@@ -0,0 +1,95 @@
+import itertools
+import os.path
+import subprocess
+from collections import defaultdict
+from io import StringIO
+
+import matplotlib.pyplot as plt
+from IPython.display import Markdown, display
+
+
+class Report:
+    """A report that is gradually built up.
+    
+    The idea is that an object like this is created at the beginning of
+    a notebook, and various cells report various things, generating plots
+    and writing markdown/math output both to the report and the notebook
+    cells. At the end the report can be rendered to PDF.
+    
+    The report is organized into "sections"; each section is appended to
+    by successive cells, and the sections are emitted in the order they 
+    were first encountered. The intention is that sections can have a
+    short name to refer to them by and a long name that appears in the
+    document.
+    
+    This could probably be expanded to include page headers and footers
+    for the final PDF; PDF generation goes through pandoc, which is quite
+    configurable (and HTML might allow for such things too).
+    """
+
+    def __init__(self, sections=None):
+        self.sections = [] if sections is None else sections
+        self.section_titles = {}
+        self.section_content = defaultdict(str)
+
+    def _ensure_section(self, section):
+        if section not in self.sections:
+            self.sections.append(section)
+
+    def add_markdown(self, section, content, *, also_display=True):
+        self._ensure_section(section)
+        if also_display:
+            display(Markdown(content))
+        self.section_content[section] += "\n\n" + content
+
+    def add_verbatim(
+        self, section, content, *, highlight_language="", also_display=True
+    ):
+        new_content = f"```{highlight_language}\n{content}\n```\n"
+        self.add_markdown(section, new_content, also_display=also_display)
+
+    # I'm not sure about tables - Markdown tables are not too annoying
+    # and it's not super clear what format users would pass us tables in
+    # (Astropy Tables? lists of lists they'd have to make themselves?)
+    # https://www.markdownguide.org/extended-syntax/#tables
+
+    def _new_figure_filename(self, section):
+        for i in itertools.count(start=1):
+            name = f"figure-{section}-{i:04d}.png"
+            if not os.path.exists(name):
+                return name
+
+    def add_plot(
+        self,
+        section,
+        figure=None,
+        *,
+        caption=None,
+        also_display=False,
+        **savefig_kwargs,
+    ):
+        self._ensure_section(section)
+        # Include caption?
+        filename = self._new_figure_filename(section)
+        if figure is None:
+            figure = plt.gcf()
+        figure.savefig(filename, dpi=300, **savefig_kwargs)
+        if caption is None:
+            caption = filename
+        new_content = f"![{caption}]({filename})\n\n"
+        self.add_markdown(section, new_content, also_display=also_display)
+
+    def generate(self):
+        with StringIO() as o:
+            for s in self.sections:
+                t = self.section_titles.get(s, s)
+                print(f"\n\n# {t}\n\n", file=o)
+                print(self.section_content.get(s, "No content"), file=o)
+            return o.getvalue()
+
+    def generate_pdf(self, pdf_filename):
+        subprocess.run(
+            ["pandoc", "--from", "markdown", "--to", "pdf", "-o", pdf_filename,],
+            text=True,
+            input=self.generate(),
+        )
