3b38b893d71d628f6d398b59346724a75fe692fc
Gabriella Agazie <gabriella.agazie@gmail.com>
2022-07-26 21:21:45 +0000

    Adding freq v resid plot

--- src/timing_analysis/plot_utils.py
+++ src/timing_analysis/plot_utils.py
@@ -368,6 +368,306 @@ def plot_residuals_time(fitter, restype = 'postfit', plotsig = False, avg = Fals
 
     return
 
+
+
+def plot_residuals_freq(fitter, restype = 'postfit', plotsig = False, avg = False, whitened = False, \
+                        save = False, legend = True, title = True, axs = None, **kwargs):
+    """
+    Make a plot of the residuals vs. frequency
+
+
+    Arguments
+    ---------
+    fitter [object] : The PINT fitter object.
+    restype ['string'] : Type of residuals, pre or post fit, to plot from fitter object. Options are:
+        'prefit' - plot the prefit residuals.
+        'postfit' - plot the postfit residuals (default)
+        'both' - overplot both the pre and post-fit residuals.
+    plotsig [boolean] : If True plot number of measurements v. residuals/uncertainty, else v. residuals
+        [default: False].
+    avg [boolean] : If True and not wideband fitter, will compute and plot epoch-average residuals [default: False].
+    whitened [boolean] : If True will compute and plot whitened residuals [default: False].
+    save [boolean] : If True will save plot with the name "resid_v_freq.png" Will add averaged/whitened
+         as necessary [default: False].
+    legend [boolean] : If False, will not print legend with plot [default: True].
+    title [boolean] : If False, will not print plot title [default: True].
+    axs [string] : If not None, should be defined subplot value and the figure will be used as part of a
+         larger figure [default: None].
+
+    Optional Arguments:
+    --------------------
+    res [list/array] : List or array of residual values to plot. Will override values from fitter object.
+    errs [list/array] : List or array of residual error values to plot. Will override values from fitter object.
+    freqs [list/array] : List or array of frequencies to plot. Will override values from toa object.
+    rcvr_bcknds[list/array] : List or array of TOA receiver-backend combinations. Will override values from toa object.
+    figsize [tuple] : Size of the figure passed to matplotlib [default: (10,4)].
+    fmt ['string'] : matplotlib format option for markers [default: ('x')]
+    color ['string'] : matplotlib color option for plot [default: color dictionary in plot_utils.py file]
+    alpha [float] : matplotlib alpha options for plot points [default: 0.5]
+    """
+    # Check if wideband
+    if fitter.is_wideband:
+        NB = False
+        if avg == True:
+            raise ValueError("Cannot epoch average wideband residuals, please change 'avg' to False.")
+    else:
+        NB = True
+
+    # Check if want epoch averaged residuals
+    if avg == True and restype == 'prefit':
+        avg_dict = fitter.resids_init.ecorr_average(use_noise_model=True)
+    elif avg == True and restype == 'postfit':
+        avg_dict = fitter.resids.ecorr_average(use_noise_model=True)
+    elif avg == True and restype == 'both':
+        avg_dict = fitter.resids.ecorr_average(use_noise_model=True)
+        avg_dict_pre = fitter.resids_init.ecorr_average(use_noise_model=True)
+
+
+    # Get residuals
+    if 'res' in kwargs.keys():
+        res = kwargs['res']
+    else:
+        if restype == 'prefit':
+            if NB == True:
+                if avg == True:
+                    res = avg_dict['time_resids'].to(u.us)
+                else:
+                    res = fitter.resids_init.time_resids.to(u.us)
+            else:
+                res = fitter.resids_init.residual_objs['toa'].time_resids.to(u.us)
+        elif restype == 'postfit':
+            if NB == True:
+                if avg == True:
+                    res = avg_dict['time_resids'].to(u.us)
+                else:
+                    res = fitter.resids.time_resids.to(u.us)
+            else:
+                res = fitter.resids.residual_objs['toa'].time_resids.to(u.us)
+        elif restype == 'both':
+            if NB == True:
+                if avg == True:
+                    res = avg_dict['time_resids'].to(u.us)
+                    res_pre = avg_dict_pre['time_resids'].to(u.us)
+                else:
+                    res = fitter.resids.time_resids.to(u.us)
+                    res_pre = fitter.resids_init.time_resids.to(u.us)
+            else:
+                res = fitter.resids.residual_objs['toa'].time_resids.to(u.us)
+                res_pre = fitter.resids_init.residual_objs['toa'].time_resids.to(u.us)
+        else:
+            raise ValueError("Unrecognized residual type: %s. Please choose from 'prefit', 'postfit', or 'both'."\
+                             %(restype))
+
+    # Check if we want whitened residuals
+    if whitened == True and ('res' not in kwargs.keys()):
+        if avg == True:
+            if restype != 'both':
+                res = whiten_resids(avg_dict, restype=restype)
+            else:
+                res = whiten_resids(avg_dict_pre, restype='prefit')
+                res_pre = whiten_resids(avg_dict, restype='postfit')
+                res_pre = res_pre.to(u.us)
+            res = res.to(u.us)
+        else:
+            if restype != 'both':
+                res = whiten_resids(fitter, restype=restype)
+            else:
+                res = whiten_resids(fitter, restype='prefit')
+                res_pre = whiten_resids(fitter, restype='postfit')
+                res_pre = res_pre.to(u.us)
+            res = res.to(u.us)
+
+    # Get errors
+    if 'errs' in kwargs.keys():
+        errs = kwargs['errs']
+    else:
+        if restype == 'prefit':
+            if avg == True:
+                errs = avg_dict['errors'].to(u.us)
+            else:
+                errs = fitter.toas.get_errors().to(u.us)
+        elif restype == 'postfit':
+            if NB == True:
+                if avg == True:
+                    errs = avg_dict['errors'].to(u.us)
+                else:
+                    errs = fitter.resids.get_data_error().to(u.us)
+            else:
+                errs = fitter.resids.residual_objs['toa'].get_data_error().to(u.us)
+        elif restype == 'both':
+            if NB == True:
+                if avg == True:
+                    errs = avg_dict['errors'].to(u.us)
+                    errs_pre = avg_dict_pre['errors'].to(u.us)
+                else:
+                    errs = fitter.resids.get_data_error().to(u.us)
+                    errs_pre = fitter.toas.get_errors().to(u.us)
+            else:
+                errs = fitter.resids.residual_objs['toa'].get_data_error().to(u.us)
+                errs_pre = fitter.toas.get_errors().to(u.us)
+
+    # Get freqs
+    if 'freqs' in kwargs.keys():
+        freqs = kwargs['freqs']
+    else:
+        freqs = fitter.toas.get_freqs().value
+
+    # Get receiver backends
+    if 'rcvr_bcknds' in kwargs.keys():
+        rcvr_bcknds = kwargs['rcvr_bcknds']
+    else:
+        rcvr_bcknds = np.array(fitter.toas.get_flag_value('f')[0])
+        if avg == True:
+            avg_rcvr_bcknds = []
+            for iis in avg_dict['indices']:
+                avg_rcvr_bcknds.append(rcvr_bcknds[iis[0]])
+            rcvr_bcknds = np.array(avg_rcvr_bcknds)
+    # Get the set of unique receiver-bandend combos
+    RCVR_BCKNDS = set(rcvr_bcknds)
+
+    if 'figsize' in kwargs.keys():
+        figsize = kwargs['figsize']
+    else:
+        figsize = (10,4)
+    if axs == None:
+        fig = plt.figure(figsize=figsize)
+        ax1 = fig.add_subplot(111)
+    else:
+        fig = plt.gcf()
+        ax1 = axs
+    for i, r_b in enumerate(RCVR_BCKNDS):
+        inds = np.where(rcvr_bcknds==r_b)[0]
+        if not inds.tolist():
+            r_b_label = ""
+        else:
+            r_b_label = rcvr_bcknds[inds][0]
+        # Get plot preferences
+        if 'fmt' in kwargs.keys():
+            mkr = kwargs['fmt']
+        else:
+            mkr = markers[r_b_label]
+            if restype == 'both':
+                mkr_pre = '.'
+        if 'color' in kwargs.keys():
+            clr = kwargs['color']
+        else:
+            clr = colorscheme[r_b_label]
+        if 'alpha' in kwargs.keys():
+            alpha = kwargs['alpha']
+        else:
+            alpha = 0.5
+        if plotsig:
+            sig = res[inds]/errs[inds]
+            ax1.errorbar(freqs[inds], sig, yerr=len(errs[inds])*[1], fmt=mkr, \
+                     color=clr, label=r_b_label, alpha = alpha, picker=True)
+            if restype == 'both':
+                sig_pre = res_pre[inds]/errs_pre[inds]
+                ax1.errorbar(freqs[inds], sig_pre, yerr=len(errs_pre[inds])*[1], fmt=mkr_pre, \
+                         color=clr, label=r_b_label+" Prefit", alpha = alpha, picker=True)
+        else:
+            ax1.errorbar(freqs[inds], res[inds], yerr=errs[inds], fmt=mkr, \
+                     color=clr, label=r_b_label, alpha = alpha, picker=True)
+            if restype == 'both':
+                ax1.errorbar(freqs[inds], res_pre[inds], yerr=errs_pre[inds], fmt=mkr_pre, \
+                     color=clr, label=r_b_label+" Prefit", alpha = alpha, picker=True)
+
+    # Set second axis
+    ax1.set_xlabel(r'Frequncy (MHz)')
+    ax1.grid(True)
+    ax2 = ax1.twiny()
+    freq0 = freqs.min()
+    freq1 = freqs.max()
+    ax2.set_xlim(freq0, freq1)
+    if plotsig:
+        if avg and whitened:
+            ax1.set_ylabel('Average Residual/Uncertainty \n (Whitened)', multialignment='center')
+        elif avg and not whitened:
+            ax1.set_ylabel('Average Residual/Uncertainty')
+        elif whitened and not avg:
+            ax1.set_ylabel('Residual/Uncertainty \n (Whitened)', multialignment='center')
+        else:
+            ax1.set_ylabel('Residual/Uncertainty')
+    else:
+        if avg and whitened:
+            ax1.set_ylabel('Average Residual ($\mu$s) \n (Whitened)', multialignment='center')
+        elif avg and not whitened:
+            ax1.set_ylabel('Average Residual ($\mu$s)')
+        elif whitened and not avg:
+            ax1.set_ylabel('Residual ($\mu$s) \n (Whitened)', multialignment='center')
+        else:
+            ax1.set_ylabel('Residual ($\mu$s)')
+    if legend:
+        if len(RCVR_BCKNDS) > 5:
+            ncol = int(np.ceil(len(RCVR_BCKNDS)/2))
+            y_offset = 1.15
+        else:
+            ncol = len(RCVR_BCKNDS)
+            y_offset = 1.0
+        ax1.legend(loc='upper center', bbox_to_anchor= (0.5, y_offset+1.0/figsize[1]), ncol=ncol)
+    if title:
+        if len(RCVR_BCKNDS) > 5:
+            y_offset = 1.1
+        else:
+            y_offset = 1.0
+        plt.title("%s %s frequency residuals" % (fitter.model.PSR.value, restype), y=y_offset+1.0/figsize[1])
+    if axs == None:
+        plt.tight_layout()
+    if save:
+        ext = ""
+        if whitened:
+            ext += "_whitened"
+        if avg:
+            ext += "_averaged"
+        if NB:
+            ext += "_NB"
+        else:
+            ext += "_WB"
+        if restype == 'prefit':
+            ext += "_prefit"
+        elif restype == 'postfit':
+            ext += "_postfit"
+        elif restype == "both":
+            ext += "_pre_post_fit"
+        plt.savefig("%s_resid_v_freq%s.png" % (fitter.model.PSR.value, ext))
+    
+    if axs == None:
+        # Define clickable points
+        text = ax2.text(0,0,"")
+
+        # Define point highlight color
+        if "430_ASP" in RCVR_BCKNDS or "430_PUPPI" in RCVR_BCKNDS:
+            stamp_color = "#61C853"
+        else:
+            stamp_color = "#FD9927"
+
+        def onclick(event):
+            # Get X and Y axis data
+            xdata = freqs
+            if plotsig:
+                ydata = (res/errs).decompose().value
+            else:
+                ydata = res.value
+            # Get x and y data from click
+            xclick = event.xdata
+            yclick = event.ydata
+            # Calculate scaled distance, find closest point index
+            d = np.sqrt(((xdata - xclick)/10.0)**2 + (ydata - yclick)**2)
+            ind_close = np.where(np.min(d) == d)[0]
+            # highlight clicked point
+            ax2.scatter(xdata[ind_close], ydata[ind_close], marker = 'x', c = stamp_color)
+            # Print point info
+            text.set_position((xdata[ind_close], ydata[ind_close]))
+            if plotsig:
+                text.set_text("TOA Params:\n Frequency: %s \n Res/Err: %.2f \n Index: %s" % (xdata[ind_close][0], ydata[ind_close], ind_close[0]))
+            else:
+                text.set_text("TOA Params:\n Frequency: %s \n Res: %.2f \n Index: %s" % (xdata[ind_close][0], ydata[ind_close], ind_close[0]))
+
+        fig.canvas.mpl_connect('button_press_event', onclick)
+
+    return
+
+
+
 def plot_dmx_time(fitter, savedmx = False, save = False, legend = True,\
                   axs = None, title = True, compare = False, **kwargs):
     """
