b408142def81eb43fb71a176025cc4860fd32183
Ross Jennings <rossjjennings@gmail.com>
2022-12-07 18:01:57 -0500

    Auto-update YAML; move and log when appropriate

--- src/pint_pal/checkin.py
+++ src/pint_pal/checkin.py
@@ -1,4 +1,5 @@
 from pint_pal.yamlio import read_yaml, write_yaml
+from astropy import log
 import os
 import glob
 import shutil
@@ -14,22 +15,53 @@ def shuffle_pars(tc):
     ----------
     tc: TimingConfiguration object derived from the YAML configuration file.
     """
-    yaml_file = tc.filename
+    # Get file and repo locations
     par_dir = tc.par_directory
     archive_dir = os.path.join(par_dir, "archive")
     repo = git.Repo(par_dir, search_parent_directories=True)
 
     timing_model = tc.get_model_path()
     timing_model_basename = os.path.split(timing_model)[1]
+    new_compare_model = os.path.join(archive_dir, timing_model_basename)
 
+    # Get path to new par file
     # Assummes the newest parfile in the working directory is the correct one
     new_par = max(glob.iglob("*.par"), key=os.path.getctime)
     new_par_basename = os.path.split(new_par)[1]
-
-    new_compare_model = os.path.join(archive_dir, timing_model_basename)
-    shutil.copy(timing_model, new_compare_model)
-    repo.index.add(os.path.join(os.getcwd(), new_compare_model))
-
     new_timing_model = os.path.join(par_dir, new_par_basename)
+
+    # Make sure there are no uncommitted changes to the current timing-model
+    head = repo.commit("HEAD")
+    tracked_files = set(item.path for item in head.tree.traverse())
+    diff = head.diff(None) # diff between HEAD and current working directory state
+    # A file is changed if it's on either end of a diff entry
+    changed_files = set(item.a_path for item in diff)
+    changed_files |= set(item.b_path for item in diff)
+    
+
+    if timing_model not in tracked_files:
+        log.info(f"Par file to be archived ({timing_model}) is not tracked. "
+                 f"Copying it to {archive_dir} and leaving the original.")
+        shutil.copy(timing_model, new_compare_model)
+        repo.index.add(os.path.join(os.getcwd(), new_compare_model))
+    elif timing_model in changed_files:
+        log.warning(f"Par file to be archived ({timing_model}) has uncommitted changes. "
+                    "Leaving it alone for now. Please resolve before committing "
+                    "the new timing solution.")
+    else:
+        log.info(f"Moving {timing_model} to {archive_dir}")
+        shutil.move(timing_model, new_compare_model, copy_function=shutil.copy)
+        repo.index.add(os.path.abspath(new_compare_model))
+
+    log.info(f"Copying {new_par} to {par_dir}")             
     shutil.copy(new_par, new_timing_model)
-    repo.index.add(os.path.join(os.getcwd(), new_timing_model))
+    repo.index.add(os.path.abspath(new_timing_model))
+    
+    # Update YAML configuration accordingly
+    config_path = os.path.abspath(tc.filename)
+    config = read_yaml(config_path)
+    config['timing-model'] = os.path.relpath(new_timing_model, start=par_dir)
+    config['compare-model'] = os.path.relpath(new_compare_model, start=par_dir)
+    write_yaml(config, config_path)
+    repo.index.add(config_path)
+ 
