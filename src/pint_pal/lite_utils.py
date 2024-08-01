573f281d7dd8cb1defd0e78c70d47f8d916e3538
Jamison Talley <114320758+JamisonTalley@users.noreply.github.com>
2024-08-01 12:05:47 -0600

    Update lite_utils.py

--- src/pint_pal/lite_utils.py
+++ src/pint_pal/lite_utils.py
@@ -1424,28 +1424,31 @@ def check_convergence(fitter):
     Parameters
     ==========
     fitter: `pint.fitter` object
+
+    Returns
+    =======
+    output_val: string object that corresponds to the printed outputs of the checker
     """
     chi2_decrease = fitter.resids_init.chi2-fitter.resids.chi2
-    decrease = str(f"chi-squared decreased during fit by {chi2_decrease}")
-    print(decrease)
-    convergence = ""
+    output_val = str(f"chi-squared decreased during fit by {chi2_decrease}")
+    print(f"chi-squared decreased during fit by {chi2_decrease}")
     if hasattr(fitter, "converged") and fitter.converged:
-        convergence = "Fitter has converged"
-        print(convergence)
+        print("Fitter has converged")
+        output_val.append("\nFitter has converged")
     else:
         if abs(chi2_decrease)<0.01:
-            convergence = "Fitter has probably converged"
-            print(convergence)
+            print("Fitter has probably converged")
+            output_val.append("\nFitter has probably converged")
         elif chi2_decrease<0:
             log.warning("Fitter has increased chi2!")
-            convergence = "Fitter has increased chi2!"
+            output_val.append("\nFitter has increased chi2!")
         else:
             log.warning("Fitter may not have converged")
-            convergence = "Fitter may not have converged"
+            output_val.append("\nFitter may not have converged")
     if chi2_decrease > 0.01:
         log.warning("Input par file is not fully fitted")
-        convergence = "Input par file is not fully fitted"
-    return decrease,convergence
+        output_val.append("\nInput par file is not fully fitted")
+    return output_val
 
 def file_look(filenm, plot_type = 'profile'):
     """ Plots profile, GTpd, or YFp for a single file
