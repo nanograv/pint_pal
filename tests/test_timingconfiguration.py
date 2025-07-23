74d9bfd88962bdf31817847cad30acdbf5ad04b9
Michael Lam <michael.lam@nanograv.org>
2025-07-23 17:03:33 -0400

    Fix relative paths for test_timingconfiguration

--- tests/test_timingconfiguration.py
+++ tests/test_timingconfiguration.py
@@ -5,13 +5,24 @@ These tests are performed on YAML configuration files
 '''
 
 import pytest
+from pathlib import Path
 from pint_pal.timingconfiguration import TimingConfiguration
 
 
 @pytest.fixture
 def tc():
-    """ Load a working TimingConfiguration object for testing """
-    return TimingConfiguration("configs/J0605+3757.nb.yaml")
+    """
+    Load a working TimingConfiguration object for testing
+
+    To allow for running tests outside of tests/, the
+    par and tim directories are overwritten
+    """
+    parent = Path(__file__).parent
+    par_directory = parent / "results/"
+    tim_directory = parent / "tim/" 
+    configfile = parent / "configs/J0605+3757.nb.yaml"
+    print(par_directory, configfile)
+    return TimingConfiguration(configfile, tim_directory=tim_directory, par_directory=par_directory)
 @pytest.fixture
 def PSR():
     return "J0605+3757"
