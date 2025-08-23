f6c41f1cd2054ef7d537c49a1f6f1f6e11a9a860
Michael Lam <michael.lam@nanograv.org>
2025-08-23 03:28:08 -0400

    highly streamline config.py to avoid additional naming of new variables

--- src/pint_pal/config.py
+++ src/pint_pal/config.py
@@ -43,48 +43,8 @@ def read_config_file(config_file):
     with open(config_file, 'r') as f:
         config = yaml.load(f)
 
-    global RUN_FTEST
-    global RUN_NOISE_ANALYSIS
-    global CHECK_EXCISION
-    global USE_EXISTING_NOISE_DIR
-    global USE_TOA_PICKLE
-    global LOG_LEVEL
-    global LOG_TO_FILE
-    global LATEST_BIPM
-    global LATEST_EPHEM
-    global PLANET_SHAPIRO
-    global CORRECT_TROPOSPHERE
-    global FREQUENCY_RATIO
-    global MAX_SOLARWIND_DELAY
-    global LATEST_TOA_RELEASE
+    for key in config.keys():
+        globals()[key] = config[key]
 
-    if 'RUN_FTEST' in config:
-        RUN_FTEST = config['RUN_FTEST']
-    if 'RUN_NOISE_ANALYSIS' in config:
-        RUN_NOISE_ANALYSIS = config['RUN_NOISE_ANALYSIS']
-    if 'CHECK_EXCISION' in config:
-        CHECK_EXCISION = config['CHECK_EXCISION']
-    if 'USE_EXISTING_NOISE_DIR' in config:
-        USE_EXISTING_NOISE_DIR = config['USE_EXISTING_NOISE_DIR']
-    if 'USE_TOA_PICKLE' in config:
-        USE_TOA_PICKLE = config['USE_TOA_PICKLE']
-    if 'LOG_LEVEL' in config:
-        LOG_LEVEL = config['LOG_LEVEL']
-    if 'LOG_TO_FILE' in config:
-        LOG_TO_FILE = config['LOG_TO_FILE']
-    if 'LATEST_BIPM' in config:
-        LATEST_BIPM = config['LATEST_BIPM']
-    if 'LATEST_EPHEM' in config:
-        LATEST_EPHEM = config['LATEST_EPHEM']
-    if 'PLANET_SHAPIRO' in config:
-        PLANET_SHAPIRO = config['PLANET_SHAPIRO']
-    if 'CORRECT_TROPOSPHERE' in config:
-        CORRECT_TROPOSPHERE = config['CORRECT_TROPOSPHERE']
-    if 'FREQUENCY_RATIO' in config:
-        FREQUENCY_RATIO = config['FREQUENCY_RATIO']
-    if 'MAX_SOLARWIND_DELAY' in config:
-        MAX_SOLARWIND_DELAY = config['MAX_SOLARWIND_DELAY']
-    if 'LATEST_TOA_RELEASE' in config:
-        LATEST_TOA_RELEASE = config['LATEST_TOA_RELEASE']
 
 read_config_file(os.path.join(PACKAGE_DIR, 'defaults.yaml'))
