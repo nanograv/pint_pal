8684c7fcc579b404c2e0864b0ef56fb7bcde5984
Ross Jennings <rossjjennings@gmail.com>
2025-09-03 16:25:44 -0400

    reset_data_root -> reset_config

--- src/pint_pal/__init__.py
+++ src/pint_pal/__init__.py
@@ -1,5 +1,5 @@
 import pint_pal.packageconfiguration # must go before any use of pint_pal.config
-from pint_pal.packageconfiguration import set_data_root, reset_data_root
+from pint_pal.packageconfiguration import set_data_root, reset_config
 import pint_pal.checkin
 
 from . import _version
