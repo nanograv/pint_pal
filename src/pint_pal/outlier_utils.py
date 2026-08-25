2188c0e842be8a0ebcae9563824e224adf950f31
Jeremy Baier <baierj@oregonstate.edu>
2026-08-25 16:56:02 -0700

    update outlier utils to work with new workflow of discovery outliers

--- src/pint_pal/outlier_utils.py
+++ src/pint_pal/outlier_utils.py
@@ -9,7 +9,8 @@ from multiprocessing import Pool
 from pint.fitter import ConvergenceFailure
 import copy
 from scipy.special import fdtr
-from pint_pal.utils import apply_cut_flag, apply_cut_select
+from pint_pal.noise_utils import core_from_feather
+from pint_pal.utils import apply_cut_flag, apply_cut_select, apply_cut_select
 from pint_pal.lite_utils import write_tim
 from pint_pal.dmx_utils import *
 
@@ -315,36 +316,41 @@ def get_entPintPulsar(model,toas,sort=False,drop_pintpsr=True):
     from enterprise.pulsar import PintPulsar
     return PintPulsar(toas,model,sort=sort,drop_pintpsr=drop_pintpsr)
 
-def calculate_pout(model, toas, tc_object):
-    """Determines TOA outlier probabilities using choices specified in the
-    timing configuration file's outlier block. Write tim file with pout flags/values.
+def calculate_pout(model, toas, tc_object, feather_path, avg_method = 'mean'):
+    """Calculates and applies outlier probabilities to TOAs.
 
     Parameters
     ==========
     model: `pint.model.TimingModel` object
     toas: `pint.toa.TOAs` object
     tc_object: `pint_pal.timingconfiguration` object
+    feather_path: `pathlib.Path` object
+    avg_method: string, optional
+        Accepted values are 'mean' or 'median'. Defaults to 'mean'
     """
-    method = tc_object.get_outlier_method()
-    results_dir = f'outlier/{tc_object.get_outfile_basename()}'
-    Nsamples = tc_object.get_outlier_samples()
-    Nburnin = tc_object.get_outlier_burn()
 
-    if method == 'hmc':
-        epp = get_entPintPulsar(model, toas, drop_pintpsr=False)
-        from enterprise_outliers.hmc_outlier import OutlierHMC
-        pout = OutlierHMC(epp, outdir=results_dir, Nsamples=Nsamples, Nburnin=Nburnin)
-        print('') # Progress bar doesn't print a newline
-        # Some sorting will be needed here so pout refers to toas order?
-    elif method == 'gibbs':
-        epp = get_entPintPulsar(model, toas)
-        pout = gibbs_run(epp,results_dir=results_dir,Nsamples=Nsamples)
+    results_dir = f'outlier/{tc_object.get_outfile_basename()}'
+    method = tc_object.get_outlier_method()
+
+    #Read in feather file into a core object
+    c0 = core_from_feather(feather_path)
+    z_i = [p for p in c0.params if 'z_i' in p]
+
+    #Calculate pout via avging method for each z_i
+    pout = np.zeros(len(z_i))
+    if avg_method == 'mean':
+        for i in range(len(z_i)):
+            pout[i] = np.mean(c0(z_i[i]))
+    elif avg_method == 'median':
+        for i in range(len(z_i)):
+            pout[i] = np.median(c0(z_i[i]))
     else:
-        log.error(f'Specified method ({method}) is not recognized.')
+        raise ValueError("avg_method must be either 'mean' or 'median'")
 
     # Apply pout flags, cuts
     for i,oi in enumerate(toas.table['index']):
         toas.orig_table[oi]['flags'][f'pout_{method}'] = str(pout[i])
+
 
     # Re-introduce cut TOAs for writing tim file that includes -cut/-pout flags
     toas.table = toas.orig_table
@@ -355,27 +361,35 @@ def calculate_pout(model, toas, tc_object):
     # Need to mask TOAs once again
     apply_cut_select(toas,reason='resumption after write_tim, pout')
 
-def make_pout_cuts(model,toas,tc_object,outpct_threshold=8.0):
+def make_pout_cuts(model, toas, tc_object, outpct_threshold=8.0, save=True, save_dir='outlier'):
     """Apply cut flags to TOAs with outlier probabilities larger than specified threshold.
-    Also runs setup_dmx.
+    By default saves cut flags into a .tim file.
 
     Parameters
     ==========
+    model: `pint.model.TimingModel` object
     toas: `pint.toa.TOAs` object
     tc_object: `pint_pal.timingconfiguration` object
     outpct_threshold: float, optional
-       cut file's remaining TOAs (maxout) if X% were flagged as outliers (default set by 5/64=8%) 
+        cut file's remaining TOAs (maxout) if X% were flagged as outliers (default set by 5/64=8%)
+    save: bool, optional
+        save the cuts to a .tim file
+    save_dir: string, optional
     """
     toas = tc_object.apply_ignore(toas,specify_keys=['prob-outlier'])
     apply_cut_select(toas,reason='outlier analysis, specified key')
-    toas = setup_dmx(model,toas,frequency_ratio=tc_object.get_fratio(),max_delta_t=tc_object.get_sw_delay())
 
     # Now cut files if X% or more TOAs/file are flagged as outliers
     if tc_object.get_toa_type() == 'NB':
         tc_object.check_file_outliers(toas,outpct_threshold=outpct_threshold)
-        toas = setup_dmx(model,toas,frequency_ratio=tc_object.get_fratio(),max_delta_t=tc_object.get_sw_delay())
     else:
         log.info('Skipping maxout cuts (wideband).')
+
+    if save:
+        toas.table = toas.orig_table
+        fo = tc_object.construct_fitter(toas,model)
+        cuts_timfile = f'{save_dir}/{tc_object.get_outfile_basename()}_pout_cuts.tim'
+        write_tim(fo,toatype=tc_object.get_toa_type(),outfile=cuts_timfile)
 
 def Ftest(chi2_1, dof_1, chi2_2, dof_2):
     """
